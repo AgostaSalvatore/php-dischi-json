4c91ffa01177c9440c10f65ad3e7e0bb71409e5d
AgostaSalvatore <agosta.boolean@gmail.com>
2025-07-21 14:07:16 +0200

    feat: create songs JSON data and implement PHP display with Bootstrap cards

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-
+$cd  = file_get_contents('./songs.json');
+$cds = json_decode($cd, true);
 ?>
 
 <!DOCTYPE html>
@@ -7,9 +8,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <title>Dischi PHP</title>
 </head>
 <body>
-    
+    <h1>
+        Dischi PHP
+    </h1>
+
+    <div class="container">
+        <?php
+        foreach ($cds as $song) {
+            ?>
+        <div class="card border-dark mb-3">
+            <div class="card-title p-3">
+                <?php
+                echo $song['title'];
+                ?>
+            </div>
+            <div class="card-body">
+                <?php
+                echo $song['author'] . '<br>';
+                echo $song['year'] . '<br>';
+                echo $song['genre'] . '<br>';
+                ?>
+            </div>
+        </div>
+        <?php
+        }
+        ?>
+    </div>
 </body>
 </html>
